574b9ce7c67c43c63bee2cbc4afbd309ee1715f4
azadreg8 <regragui8@hotmail.com>
2022-08-08 16:40:41 +0200

    export base de donnée fin projet

--- REGRAGUI_SHEHERAZADE_2_EXPORTATION_BASE_DE_ DONNEES_SQL_082022.sql
+++ REGRAGUI_SHEHERAZADE_2_EXPORTATION_BASE_DE_ DONNEES_SQL_082022.sql
@@ -1,7 +1,14 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
 -- Hôte : localhost
--- Généré le :  Dim 31 juillet. 2021 à 02H33
+-- Généré le : lun. 08 août 2022 à 15:33
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -11,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données :  `GBAFBANQUE`
+-- Base de données : `GBAFBANQUE`
 --
 
 -- --------------------------------------------------------
@@ -32,9 +39,9 @@ CREATE TABLE `acteurs` (
 --
 
 INSERT INTO `acteurs` (`id_acteur`, `acteur`, `description`, `logo`) VALUES
-(1, 'Formation&co', 'Formation&co est une association française présente sur tout le territoire.\r\nNous proposons à des personnes issues de tout milieu de devenir entrepreneur grâce à un crédit et un accompagnement professionnel et personnalisé.<br/><br/>\r\nNotre proposition :<br/>\r\n\r\nun financement jusqu’à 30 000€ ;<br/>\r\nun suivi personnalisé et gratuit ;<br/>\r\nune lutte acharnée contre les freins sociétaux et les stéréotypes.<br/><br/>\r\n\r\nLe financement est possible, peu importe le métier : coiffeur, banquier, éleveur de chèvres… . <br/>\r\nNous collaborons avec des personnes talentueuses et motivées.<br/>\r\nVous n’avez pas de diplômes ? Ce n’est pas un problème pour nous ! \r\nNos financements s’adressent à tous.', 'img/formation_co.png'),
+(1, 'Formation&co', 'Formation&co est une association française présente sur tout le territoire.\r\nNous proposons à des personnes issues de tout milieu de devenir entrepreneur grâce à un crédit et un accompagnement professionnel et personnalisé.<br/><br/>\r\nNotre proposition :<br/>\r\n\r\nun financement jusqu’à 30 000€ ;<br/>\r\nun suivi personnalisé et gratuit ;<br/>\r\nune lutte acharnée contre les freins sociétaux et les stéréotypes.<br/><br/>\r\n\r\nLe financement est possible, peu importe le métier : coiffeur, banquier, éleveur de chèvres… . <br/>\r\nNous collaborons avec des personnes talentueuses et motivées.<br/>\r\nVous n’avez pas de diplômes ? Ce n’est pas un problème pour nous ! \r\nNos financements s’adressent à tous.', 'img/formationco.png'),
 (2, 'Protectpeople', 'Protectpeople finance la solidarité nationale.\r\nNous appliquons le principe édifié par la Sécurité sociale française en 1945 : permettre à chacun de bénéficier d’une protection sociale.<br/><br/>\r\n\r\nChez Protectpeople, chacun cotise selon ses moyens et reçoit selon ses besoins.<br/>\r\nProectecpeople est ouvert à tous, sans considération d’âge ou d’état de santé.<br/>\r\nNous garantissons un accès aux soins et une retraite.<br/><br/>\r\nChaque année, nous collectons et répartissons 300 milliards d’euros.<br/><br/>\r\nNotre mission est double :<br/>\r\nsociale : nous garantissons la fiabilité des données sociales ;<br/>\r\néconomique : nous apportons une contribution aux activités économiques.', 'img/protectpeople.png'),
-(3, 'DSA France', 'Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales.<br/>\r\nNous accompagnons les entreprises dans les étapes clés de leur évolution.<br/><br/>\r\nNotre philosophie : s’adapter à chaque entreprise.<br/><br/>\r\nNous les accompagnons pour voir plus grand et plus loin et proposons des solutions de financement adaptées à chaque étape de la vie des entreprises.', 'img/Dsa_france.png'),
+(3, 'DSA France', 'Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales.<br/>\r\nNous accompagnons les entreprises dans les étapes clés de leur évolution.<br/><br/>\r\nNotre philosophie : s’adapter à chaque entreprise.<br/><br/>\r\nNous les accompagnons pour voir plus grand et plus loin et proposons des solutions de financement adaptées à chaque étape de la vie des entreprises.', 'img/Dsafrance.png'),
 (4, 'CDE', 'La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation.\r\nSon président est élu pour 3 ans par ses pairs, chefs d’entreprises et présidents des CDE.', 'img/CDE.png');
 
 -- --------------------------------------------------------
@@ -56,10 +63,11 @@ CREATE TABLE `comments` (
 --
 
 INSERT INTO `comments` (`id_comment`, `user_id`, `acteur_id`, `date_add`, `comment`) VALUES
-(26, 2, 1, '2022-07-26', 'Commentaire test Formation&amp;co'),
-(27, 2, 2, '2022-07-26', 'Commentaire test Protectpeople'),
-(28, 2, 3, '2022-07-26', 'Commentaire test DSA'),
-(29, 2, 4, '2022-07-26', 'Commentaire test CDE');
+(26, 2, 1, '2022-07-26', 'super comme entreprise'),
+(27, 2, 2, '2022-07-26', 'nul Protectpeople'),
+(28, 2, 3, '2022-07-26', 'sans commentaire DSA'),
+(29, 2, 4, '2022-07-26', 'a tester CDE'),
+(30, 8, 1, '2022-08-02', 'ko');
 
 -- --------------------------------------------------------
 
@@ -85,6 +93,20 @@ CREATE TABLE `likes` (
   `acteur_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Déchargement des données de la table `likes`
+--
+
+INSERT INTO `likes` (`id_like`, `user_id`, `acteur_id`) VALUES
+(67, 8, 3),
+(68, 9, 2),
+(70, 9, 1),
+(71, 9, 4),
+(72, 2, 1),
+(73, 2, 2),
+(74, 2, 3),
+(76, 2, 4);
+
 -- --------------------------------------------------------
 
 --
@@ -106,7 +128,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id_user`, `nom`, `prenom`, `username`, `password`, `question_secrete`, `reponse_secrete`) VALUES
-(2, 'regragui', 'sheherazade', 'azadreg', 'shishou', 'le nom de jeune fille de votre mère', 'fatima');
+(2, 'regr', 'she', 'azadreg', '$2y$10$7M07zxIjjfF0lJqAaRpAH.EzjRGkwuFR99UsbH1atYDOtBfWXRkYq', 'choix1', 'jenna'),
+(8, 'regragui', 'sheherazade', 'azad', '$2y$10$ypKZpZp7oUgQT4Xl8fRyGeRnTN1Cdsw70QcSlVDYY.fbB0JsX74/S', 'choix1', 'benmassaoud'),
+(9, 'arfaoui', 'jenna', 'jennoh', '$2y$10$TYMPxKDN2sP0bhmgFOV8/OyVNBoRtpyHVo01qS9YX4F2Xo9F1mlnG', 'choix1', 'regragui');
 
 --
 -- Index pour les tables déchargées
@@ -137,7 +161,7 @@ ALTER TABLE `dislikes`
 -- Index pour la table `likes`
 --
 ALTER TABLE `likes`
-  ADD PRIMARY KEY (`id_like`),
+  ADD PRIMARY KEY (`id_like`) USING BTREE,
   ADD KEY `like_acteur` (`acteur_id`),
   ADD KEY `like_user` (`user_id`);
 
@@ -161,25 +185,25 @@ ALTER TABLE `acteurs`
 -- AUTO_INCREMENT pour la table `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `id_comment` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id_comment` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT pour la table `dislikes`
 --
 ALTER TABLE `dislikes`
-  MODIFY `id_dislike` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id_dislike` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT pour la table `likes`
 --
 ALTER TABLE `likes`
-  MODIFY `id_like` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+  MODIFY `id_like` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
 
 --
 -- AUTO_INCREMENT pour la table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- Contraintes pour les tables déchargées
@@ -190,6 +214,7 @@ ALTER TABLE `users`
 --
 ALTER TABLE `comments`
   ADD CONSTRAINT `comment_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
