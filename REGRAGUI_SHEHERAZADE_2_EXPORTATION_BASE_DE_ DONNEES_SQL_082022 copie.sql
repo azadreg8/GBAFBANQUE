2c178869edfe6b5cd01548e1fa6cc43b2b84919a
azadreg8 <regragui8@hotmail.com>
2022-08-08 15:42:32 +0200

    FIN DE PROJET NOUVELLE BASE DE DONNÉE IMPORTER PHP

---
+++ REGRAGUI_SHEHERAZADE_2_EXPORTATION_BASE_DE_ DONNEES_SQL_082022 copie.sql
@@ -0,0 +1,220 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : lun. 08 août 2022 à 15:33
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `GBAFBANQUE`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `acteurs`
+--
+
+CREATE TABLE `acteurs` (
+  `id_acteur` int(11) NOT NULL,
+  `acteur` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `logo` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `acteurs`
+--
+
+INSERT INTO `acteurs` (`id_acteur`, `acteur`, `description`, `logo`) VALUES
+(1, 'Formation&co', 'Formation&co est une association française présente sur tout le territoire.\r\nNous proposons à des personnes issues de tout milieu de devenir entrepreneur grâce à un crédit et un accompagnement professionnel et personnalisé.<br/><br/>\r\nNotre proposition :<br/>\r\n\r\nun financement jusqu’à 30 000€ ;<br/>\r\nun suivi personnalisé et gratuit ;<br/>\r\nune lutte acharnée contre les freins sociétaux et les stéréotypes.<br/><br/>\r\n\r\nLe financement est possible, peu importe le métier : coiffeur, banquier, éleveur de chèvres… . <br/>\r\nNous collaborons avec des personnes talentueuses et motivées.<br/>\r\nVous n’avez pas de diplômes ? Ce n’est pas un problème pour nous ! \r\nNos financements s’adressent à tous.', 'img/formationco.png'),
+(2, 'Protectpeople', 'Protectpeople finance la solidarité nationale.\r\nNous appliquons le principe édifié par la Sécurité sociale française en 1945 : permettre à chacun de bénéficier d’une protection sociale.<br/><br/>\r\n\r\nChez Protectpeople, chacun cotise selon ses moyens et reçoit selon ses besoins.<br/>\r\nProectecpeople est ouvert à tous, sans considération d’âge ou d’état de santé.<br/>\r\nNous garantissons un accès aux soins et une retraite.<br/><br/>\r\nChaque année, nous collectons et répartissons 300 milliards d’euros.<br/><br/>\r\nNotre mission est double :<br/>\r\nsociale : nous garantissons la fiabilité des données sociales ;<br/>\r\néconomique : nous apportons une contribution aux activités économiques.', 'img/protectpeople.png'),
+(3, 'DSA France', 'Dsa France accélère la croissance du territoire et s’engage avec les collectivités territoriales.<br/>\r\nNous accompagnons les entreprises dans les étapes clés de leur évolution.<br/><br/>\r\nNotre philosophie : s’adapter à chaque entreprise.<br/><br/>\r\nNous les accompagnons pour voir plus grand et plus loin et proposons des solutions de financement adaptées à chaque étape de la vie des entreprises.', 'img/Dsafrance.png'),
+(4, 'CDE', 'La CDE (Chambre Des Entrepreneurs) accompagne les entreprises dans leurs démarches de formation.\r\nSon président est élu pour 3 ans par ses pairs, chefs d’entreprises et présidents des CDE.', 'img/CDE.png');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id_comment` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `acteur_id` int(11) NOT NULL,
+  `date_add` date NOT NULL,
+  `comment` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `comments`
+--
+
+INSERT INTO `comments` (`id_comment`, `user_id`, `acteur_id`, `date_add`, `comment`) VALUES
+(26, 2, 1, '2022-07-26', 'super comme entreprise'),
+(27, 2, 2, '2022-07-26', 'nul Protectpeople'),
+(28, 2, 3, '2022-07-26', 'sans commentaire DSA'),
+(29, 2, 4, '2022-07-26', 'a tester CDE'),
+(30, 8, 1, '2022-08-02', 'ko');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `dislikes`
+--
+
+CREATE TABLE `dislikes` (
+  `id_dislike` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `acteur_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `likes`
+--
+
+CREATE TABLE `likes` (
+  `id_like` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `acteur_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `likes`
+--
+
+INSERT INTO `likes` (`id_like`, `user_id`, `acteur_id`) VALUES
+(67, 8, 3),
+(68, 9, 2),
+(70, 9, 1),
+(71, 9, 4),
+(72, 2, 1),
+(73, 2, 2),
+(74, 2, 3),
+(76, 2, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `users`
+--
+
+CREATE TABLE `users` (
+  `id_user` int(11) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `prenom` varchar(50) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `question_secrete` varchar(255) NOT NULL,
+  `reponse_secrete` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `users`
+--
+
+INSERT INTO `users` (`id_user`, `nom`, `prenom`, `username`, `password`, `question_secrete`, `reponse_secrete`) VALUES
+(2, 'regr', 'she', 'azadreg', '$2y$10$7M07zxIjjfF0lJqAaRpAH.EzjRGkwuFR99UsbH1atYDOtBfWXRkYq', 'choix1', 'jenna'),
+(8, 'regragui', 'sheherazade', 'azad', '$2y$10$ypKZpZp7oUgQT4Xl8fRyGeRnTN1Cdsw70QcSlVDYY.fbB0JsX74/S', 'choix1', 'benmassaoud'),
+(9, 'arfaoui', 'jenna', 'jennoh', '$2y$10$TYMPxKDN2sP0bhmgFOV8/OyVNBoRtpyHVo01qS9YX4F2Xo9F1mlnG', 'choix1', 'regragui');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `acteurs`
+--
+ALTER TABLE `acteurs`
+  ADD PRIMARY KEY (`id_acteur`);
+
+--
+-- Index pour la table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id_comment`),
+  ADD KEY `comment_user` (`user_id`);
+
+--
+-- Index pour la table `dislikes`
+--
+ALTER TABLE `dislikes`
+  ADD PRIMARY KEY (`id_dislike`),
+  ADD KEY `dislike_acteur` (`acteur_id`),
+  ADD KEY `dislike_user` (`user_id`);
+
+--
+-- Index pour la table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`id_like`) USING BTREE,
+  ADD KEY `like_acteur` (`acteur_id`),
+  ADD KEY `like_user` (`user_id`);
+
+--
+-- Index pour la table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `acteurs`
+--
+ALTER TABLE `acteurs`
+  MODIFY `id_acteur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT pour la table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id_comment` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT pour la table `dislikes`
+--
+ALTER TABLE `dislikes`
+  MODIFY `id_dislike` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+
+--
+-- AUTO_INCREMENT pour la table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `id_like` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=77;
+
+--
+-- AUTO_INCREMENT pour la table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comment_user` FOREIGN KEY (`user_id`) REFERENCES `users` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
